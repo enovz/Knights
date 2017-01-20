0d62b076f1426f55ae3964e6a41fe326de4ea255
enovz <zvonimir.biocic.email@gamil.com>
2017-01-20 17:32:08 +0100

    updated data

--- src/js/data.js
+++ src/js/data.js
@@ -2,17 +2,59 @@
 /**data */
 
 export const names = [
-
+    "Candie",
+    "Marisela",
+    "Benjamin",
+    "Carlee",
+    "Rogelio",
+    "Lesli",
+    "Lorenza",
+    "Nannie",
+    "Eli",
+    "Oren",
+    "Celine",
+    "Coral",
+    "Kellie",
+    "Keisha",
+    "Janie",
+    "Stacia",
+    "Claretta",
+    "Teofila",
+    "Leatrice",
+    "Juliette"
 ];
-export const surnames= [
-
+export const surnames = [
+    "Corfas",
+    "Zapata",
+    "Cerda",
+    "Dahlstedt",
+    "Ardeni",
+    "Egan",
+    "HatchPoitras",
+    "Gottardo"
 ];
-export const stats= [
-
+export const stats = [
+    "attack",
+    "defense",
+    "stamina",
+    "courage",
+    "movement"
 ];
-export const traits= [
+export const traits = [
 
+    "adventurous","affectionate","aggressive","ambitious","angry",
+    "anxious","apologetic","arrogant","articulate","bad-mannered","bold",
+    "careless","cautious","clumsy","courageous","cowardly","cruel",
+    "dangerous","daring","depressive","devious","dishonest","disrespectful",
+    "evil","fearless","foolish","greedy","harsh","hateful","honest","impatient",
+    "impolite","inconsiderate","intelligent","jealous","judgemental","lazy",
+    "lovable","loyal","manipulative","miserable","mysterious","naive",
+    "nervous","polite","rebellious","reckless","rigid","sarcastic","selfish",
+    "shy","thoughtless","timid","unkind","unstable","untidy","wicked","wise","witty"
 ];
-export const armorParts= [
-    
+export const armorParts = [
+    "helmet",
+    "torso",
+    "arms",
+    "legs"
 ];
