f88443fd75adff19762f3ff3d17bd8d6de73733a
enovz <zvonimir.biocic.email@gamil.com>
2017-01-23 15:46:04 +0100

    updated data

--- ver2/js/helpers/data.js
+++ ver2/js/helpers/data.js
@@ -2,36 +2,17 @@
 /**data */
 
 export const names = [
-    "Candie",
-    "Marisela",
-    "Benjamin",
-    "Carlee",
-    "Rogelio",
-    "Lesli",
-    "Lorenza",
-    "Nannie",
-    "Eli",
-    "Oren",
-    "Celine",
-    "Coral",
-    "Kellie",
-    "Keisha",
-    "Janie",
-    "Stacia",
-    "Claretta",
-    "Teofila",
-    "Leatrice",
-    "Juliette"
+    "Adept", "Arcanist", "Archmage", "Archer", "Assassin", "Battlemage", "Bard", "Beastmaster", "Binder", "Bladesinger", "Brawler", "Cavalier", "Conjurer", "Commissar", "Champion", "Cleric", "Disciple", "Divine", "Doomcaster", "Emissary", "Enchanter", "Gladiator", "Guardian", "Harbinger", "Hellreaver", "Hexblade", "Hunter", "Infiltrator", "Inquisitor", "Knight", "Lord", "Magehunter", "Marauder", "Marshal", "Master", "Navigator", "Necromancer", "Outrider", "Paladin", "Ranger", "Runecaster", "Sage", "Scavenger", "Seer", "Sentinel", "Shadowsmith", "Slayer", "Sorcerer", "Soulcaster", "Soulguard", "Spellsword", "Spymaster", "Summoner", "Sunlord", "Stormlord", "Tactician", "Theurgist", "Templar", "Warden", "Warlock", "Warmage", "Warmaster", "Warrior"
 ];
 export const surnames = [
-    "Corfas",
-    "Zapata",
-    "Cerda",
-    "Dahlstedt",
-    "Ardeni",
-    "Egan",
-    "HatchPoitras",
-    "Gottardo"
+    "Acarred", "Acartim", "Adelbard", "Adkin", "Agerrod", "Aldescord", "Aldise", "Aldyere", "Alear", "Aleden", "Aleof", "Anrie", "Ansenward", "Anstur", "Arceol", "Ardis", "Arhes", "Arynd", "Ashthan", "Athwar", "Baadel", "Baelbu", "Baldord", "Banrothy", "Barbert", "Barham", "Bawerd", "Beloh", "Belward", "Beohal", "Beorn", "Beorth", "Beorthnald", "Beosen", "Bertguth", "Bertric", "Bethlac", "Bojai", "Brihelm", "Brytris", "Buernor", "Burthae", "Cacthain", "Cangorma", "Cartondo", "Casbarhelm", "Cencyn", "Ceolwulf", "Cerzryn", "Chroage", "Cialhal", "Clacar", "Claferd", "Cloight", "Criskel", "Cuthlas", "Cymarma", "Daethan", "Danferd", "Darentri", "Dargum", "Donlett", "Dowald", "Draest", "Drakfor", "Drukthard", "Drytlas", "Dux", "Eafrid", "Eal", "Ealen", "Eawise", "Echim", "Echrad", "Edaan", "Edcar", "Edvia", "Eibryt", "Eldane", "Eldash", "Eleof", "Endtor", "Enthdar", "Erddic", "Erdra", "Eresse", "Ertai", "Estogh", "Ethelmi", "Everbrand", "Evervell", "Ferdswith", "Fermuel", "Fledgrim", "Fledmond", "Fredken", "Frid", "Fridcuth", "Fridmark", "Frith", "Frithmac", "Gaferd", "Garcrow", "Gardhol", "Gardjen", "Gardneth", "Gareon", "Garmash", "Ghaot", "Gormalett", "Grimcuth", "Grimdon", "Grimferd", "Grimwyn", "Harglor", "Hargruul", "Heald", "Helettan", "Helmsang", "Hemaroc", "Hereco", "Hesnath", "Hesylcon", "Hetimhol", "Hewbard", "Hildferd", "Hildidon", "Hildiord", "Hildired", "Hinos", "Holvel", "Honsgar", "Iannhildi", "Imaugh", "Inedas", "Ineon", "Ingchar", "Irmakethel", "Isen", "Isenson", "Issine", "Jakrahan", "Jeffdra", "Joanroc", "Jored", "Kaar", "Kahelm", "Kaltur", "Karoth", "Keciaron", "Keethel", "Kenthas", "Kerid", "Khuzme", "Kimsul", "Lacead", "Lasdra", "Lasferum", "Laumac", "Lauthgard", "Learmas", "Lenwyn", "Leof", "Leofbur", "Leofmond", "Lesealvin", "Lettfled", "Leysan", "Liamferth", "Liamhald", "Lihon", "Liron", "Locriasen", "Lormic", "Lorrod", "Lybeorn", "Lyeeng", "Lyeuld", "Malisanth", "Maordroy", "Marghel", "Markven", "Markvin", "Marmanas", "Maseph", "Mhed", "Miardas", "Mitegel", "Mondrah", "Mondvid", "Moranlor", "Morannor", "Morbur", "Moruul", "Mosorth", "Mosper", "Mudan", "Muelbert", "Muelcuth", "Naldcrow", "Naldren", "Nalech", "Nansobeth", "Naor", "Natheal", "Nathu", "Nethda", "Nethrie", "Newerd", "Niethel", "Norroth", "Nysep", "Odeb", "Ohildi", "Oldfroth", "Omolt", "Onskel", "Onwar", "Orann", "Ordjefel", "Ordlin", "Ormris", "Osnys", "Oughusk", "Queeng", "Raenvil", "Rahge", "Rahke", "Rakih", "Ranerath", "Rawyn", "Redard", "Redloc", "Redwil", "Renlett", "Retfled", "Retrah", "Riabras", "Riarroc", "Ridryt", "Rilon", "Rilrbur", "Risghat", "Risstan", "Risthor", "Risyth", "Rocrid", "Rocroy", "Rodsam", "Rolisum", "Roltim", "Ronegar", "Ronmane", "Ronvell", "Ronvithor", "Royham", "Ryess", "Ryimil", "Ryleof", "Sagardi", "Sared", "Saric", "Sathor", "Saykenth", "Shamo", "Sigand", "Sigsana", "Stanhelm", "Stipo", "Sungric", "Suroth", "Swithred", "Sylbri", "Sylwald", "Tamalor", "Terbeo", "Thaejoan", "Thobeorn", "Thorad", "Thordbur", "Thorwig", "Tomald", "Torit", "Triga", "Trisonwald", "Trivor", "Tule", "Turang", "Tyald", "Tyferth", "Tyward", "Undach", "Undarr", "Undert", "Urnret", "Ushdra", "Vagrim", "Vaser", "Ven", "Venan", "Venberbas", "Vengeor", "Verchom", "Verelm", "Vertan", "Vintada", "Voet", "Vorate", "Vorgo", "Vorrir", "Vorses", "Walddic", "Waldgrim", "Wargdorn", "Warod", "Wenferth", "Werdcas", "Werthas", "Wilannrie", "Wilvell", "Winebert", "Wiseret", "Worhin", "Wulfdun", "Wulfranth", "Wulfstan", "Wynadel", "Wyndoc", "Yerhtin", "Yerlan", "Zaarchet", "Zaregin", "Zhapol", "Zhyray", "Zoash"
+];
+
+export const traits = [
+    "Abrasive", "Aggressive", "Aimless", "Anxious", "Apologetic", "Arrogant", "Bleak", "Careless", "Cowardly", "Cruel", "Dependable", "Depressive", "Desperate", "Despairing", "Destructive", "Dishonest", "Dull", "Fanatical", "Fragile", "Fearful", "Feeble", "Foolish", "Forgetful", "Futile", "Greedy", "Grievous", "Hateful", "Humble", "Hopeless", "Unfortunate", "Uninspired", "Idealistic", "Impatient", "Ineffective", "Indecisive", "Insecure", "Irrelevant", "Jealous", "Judgemental", "Lazy", "Manipulative", "Melancholic", "Meaningless", "Miserable", "Morbid", "Mysterious", "Naive", "Nervous", "Opinionated", "Obsessive", "Passive", "Paranoid", "Pessimistic", "Powerless", "Pretentious", "Rebellious", "Reckless", "Reflective", "Reserved", "Restless", "Rigid", "Scared", "Secretive", "Selfish", "Solitary", "Sunk", "Spiritless", "Stingy", "Stubborn", "Submissive", "Thoughtless", "Timid", "Tragic", "Trivial", "Unstable", "Vain", "Volatile", "Vindictive", "Violent", "Worthless", "Withdrawn", "Weak", "Adventurous", "Ambitious", "Anxious", "Bad-mannered", "Bold", "Cautious", "Clumsy", "Courageous", "Dangerous", "Devious", "Disrespectful", "Fearless", "Honest", "Impolite", "Inconsiderate", "Intelligent", "Loyal", "Sarcastic", "Untidy"
+];
+export const skills = [
+    "Absolve", "Bargain", "Berserk", "Betray", "Bind", "Blind", "Bluff", "Break", "Burst", "Clone", "Crush", "Delude", "Destroy", "Decay", "Disguise", "Disrupt", "Dissolve", "Distract", "Dodge", "Dominate", "Elude", "Evade", "Execute", "Fade", "Focus", "Frenzy", "Fury", "Gamble", "Grasp", "Grow", "Heal", "Hex", "Hide", "Illuminate", "Imitate", "Interrupt", "Intimidate", "Leap", "Levitate", "Listen", "Maim", "Mirror", "Mislead", "Mute", "Panic", "Provoke", "Pursuit", "Rage", "Renew", "Repair", "Resolve", "Retaliate", "Rush", "Search", "Sense", "Shatter", "Sooth", "Spot", "Stun", "Sunder", "Taunt", "Torture", "Trap", "Trick", "Tumble", "Vanish", "Ward", "Weaken"
 ];
 export const stats = [
     "attack",
@@ -39,36 +20,6 @@ export const stats = [
     "stamina",
     "courage",
     "movement"
-];
-export const traits = [
-
-    "adventurous","affectionate","aggressive","ambitious","angry",
-    "anxious","apologetic","arrogant","articulate","bad-mannered","bold",
-    "careless","cautious","clumsy","courageous","cowardly","cruel",
-    "dangerous","daring","depressive","devious","dishonest","disrespectful",
-    "evil","fearless","foolish","greedy","harsh","hateful","honest","impatient",
-    "impolite","inconsiderate","intelligent","jealous","judgemental","lazy",
-    "lovable","loyal","manipulative","miserable","mysterious","naive",
-    "nervous","polite","rebellious","reckless","rigid","sarcastic","selfish",
-    "shy","thoughtless","timid","unkind","unstable","untidy","wicked","wise","witty"
-];
-export const skills =[
-    "Tracking",
-    "Boxing",
-    "Athletics",
-    "Burglary",
-    "Stealth",
-    "Tactics",
-    "Swimming",
-    "Riding",
-    "Hunting",
-    "Impersonation",
-    "Gambling",
-    "Endurance",
-    "Dual Wield",
-    "Cryptography",
-    "Climbing",
-    "Botany"
 ];
 export const armorParts = [
     "helm",
